6890573db2e2bb0895eb8c3d526e176ef9da82a0
kang <rkdgusdn0319@gmail.com>
2025-04-08 16:36:54 +0900

    commit

--- repository/db.js
+++ repository/db.js
@@ -4,10 +4,10 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'casetibuy-db.c1uagmqqwxda.ap-northeast-2.rds.amazonaws.com',
+    host: 'hyunu2000db.cfcsuo2oi4f7.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
     user: 'admin',
-    password: 'casetibuy!1344',
+    password: 'aArkdgusdn2',
     database: 'casetibuy',
     charset: 'utf8mb4'
 });
